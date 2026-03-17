e05d93e49a1e57fde7d0d5625adee7066a4e7bcf
Anna Kozakova <ankozako@shi-r11-p1.s19.be>
2026-03-17 15:08:31 +0100

    cpp wip

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -11,3 +11,32 @@
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("default"), _grade(1)
+{
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) : _name(other._name), _grade(other._grade)
+{
+	if (grade < 1)
+		throw GradeTooHighException();
+	if (grade > 150)
+		throw GradeTooLowException();
+}
+
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : _name(other._name), _grade(other._grade)
+{
+
+}
+
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+
+}
+
+
+Bureaucrat::~Bureaucrat()
+{
+}
