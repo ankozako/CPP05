224ab8e529a36c2bf284fd05856a6c6d8ee9e291
Anna Kozakova <ankozako@fu-c3-p1.s19.be>
2026-03-17 13:12:51 +0100

    setup

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ankozako <ankozako@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/17 12:02:56 by ankozako          #+#    #+#             */
+/*   Updated: 2026/03/17 12:02:59 by ankozako         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <string>
+# include <iostream>
+
+class Bureaucrat
+{
+	private:
+		const std::string _name;
+		int _grade;
+
+	public:
+		Bureaucrat();
+		Bureaucrat(std::string name, int grade);	
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat& operator=(const Bureaucrat& other);
+		~Bureaucrat();
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+
+		std::string getName() const;
+		int getGrade() const;
+};
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& b);
+
+#endif
